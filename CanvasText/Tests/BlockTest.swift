b1a22f3aa0b2cf4841934445548983f4de6e96b3
Sam Soffes <sam@soff.es>
2015-11-12 09:42:33 -0800

    Fix tests

--- CanvasText/Tests/BlockTest.swift
+++ CanvasText/Tests/BlockTest.swift
@@ -11,17 +11,15 @@ import CanvasText
 
 class BlockTest: XCTestCase {
 	func testDocHeading() {
-		let controller = TextController(text: "⧙doc-heading⧘A Lovely Document\nThis is a paragraph.\n⧙blockquote⧘> Here’s to the crazy ones…")
-		let start = controller.text.startIndex
+		let controller = TextController(backingText: "⧙doc-heading⧘A Lovely Document\nThis is a paragraph.\n⧙blockquote⧘> Here’s to the crazy ones…")
 		
-//		XCTAssertEqual(controller.lines, [
-//			Line(kind: .DocHeading, delimiter: start...start.advancedBy(12), content: start.advancedBy(13)...start.advancedBy(29)),
-//			Annotation(block: .Blockquote, delimiter: start.advancedBy(52)...start.advancedBy(64), content: start.advancedBy(64)...start.advancedBy(70))
-//		])
+		XCTAssertEqual(controller.lines, [
+			Line(kind: .DocHeading, delimiter: NSRange(location: 0, length: 13), content: NSRange(location: 13, length: 17)),
+			Line(kind: .Paragraph, content: NSRange(location: 31, length: 20)),
+			Line(kind: .Blockquote, delimiter: NSRange(location: 52, length: 12), content: NSRange(location: 64, length: 27))
+		])
 		
-		XCTAssertEqual("A Lovely Document\nThis is a paragraph.\n> Here’s to the crazy ones…", controller.displayText)
-		
-		
+		XCTAssertEqual("A Lovely Document\nThis is a paragraph.\n> Here’s to the crazy ones…", controller.displayText)		
 	}
     
 }
