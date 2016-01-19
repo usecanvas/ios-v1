b9070f457b5fa640848b0997440eaab81c580eb7
Sam Soffes <sam@soff.es>
2016-01-19 11:44:17 -0800

    Fix tint color when canceling a back drag. Fixes #106

--- Canvas/NavigationController.swift
+++ Canvas/NavigationController.swift
@@ -42,6 +42,11 @@ final class NavigationController: UINavigationController {
 
 	// MARK: - Private
 
+	private func updateTintColor(viewController: UIViewController) {
+		let tintColor = (viewController as? TintableEnvironment)?.preferredTintColor
+		updateTintColor(tintColor)
+	}
+
 	private func updateTintColor(tintColor: UIColor?) {
 		let itemsColor = tintColor ?? defaultTintColor
 		let titleColor = tintColor ?? defaultTitleColor
@@ -58,7 +63,22 @@ final class NavigationController: UINavigationController {
 
 extension NavigationController: UINavigationControllerDelegate {
 	func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool) {
-		let tintColor = (viewController as? TintableEnvironment)?.preferredTintColor
-		updateTintColor(tintColor)
+		// Call didShow if the animation is canceled
+		transitionCoordinator()?.notifyWhenInteractionEndsUsingBlock { [weak self] context in
+			guard context.isCancelled() else { return }
+			guard let delegate = self, from = context.viewControllerForKey(UITransitionContextFromViewControllerKey) else { return }
+			delegate.navigationController(navigationController, willShowViewController: from, animated: animated)
+
+			let animationCompletion = context.transitionDuration() * NSTimeInterval(context.percentComplete())
+			dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(animationCompletion) * Int64(NSEC_PER_SEC)), dispatch_get_main_queue()) {
+				delegate.navigationController(navigationController, didShowViewController: from, animated: animated)
+			}
+		}
+
+		updateTintColor(viewController)
+	}
+
+	func navigationController(navigationController: UINavigationController, didShowViewController viewController: UIViewController, animated: Bool) {
+		updateTintColor(viewController)
 	}
 }
