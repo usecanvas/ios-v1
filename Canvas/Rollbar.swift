a622a1c24771914e10ee2898c745a7c777e5556a
Sam Soffes <sam@soff.es>
2016-02-01 18:28:11 -0800

    Use sharejs-wrapper #113

--- Canvas/Rollbar.swift
+++ Canvas/Rollbar.swift
@@ -12,70 +12,69 @@ import CanvasKit
 // CanvasText web view specific Rollbar reporting.
 struct Rollbar {
 	static func report(errorMessage errorMessage: String, lineNumber: UInt? = nil, columnNumber: UInt? = nil, account: Account? = nil) {
-		// Report to Rollbar
-		let request = NSMutableURLRequest(URL: NSURL(string: "https://api.rollbar.com/api/1/item/")!)
-		request.HTTPMethod = "POST"
-
-		var data: [String: AnyObject] = [
-			"level": "error",
-			"timestamp": UInt(NSDate().timeIntervalSince1970),
-			"platform": "ios",
-			"language": "javascript",
-			"context": "editor.html",
-			"notifier": [
-				"name": "canvas-ios"
-			]
-		]
-
-		if let account = account {
-			data["person"] = [
-				"id": account.user.ID,
-				"username": account.user.username,
-				"email": account.email
-			]
-		}
-
 		#if DEBUG
-			data["environment"] = "development"
+			print("[CanvasText] JavaScript error: \(errorMessage), lineNumber: \(lineNumber), columnNumber: \(columnNumber)")
 		#else
-			data["environment"] = "production"
-		#endif
+			// Report to Rollbar
+			let request = NSMutableURLRequest(URL: NSURL(string: "https://api.rollbar.com/api/1/item/")!)
+			request.HTTPMethod = "POST"
 
-		if let lineNumber = lineNumber, columnNumber = columnNumber {
-			data["body"] = [
-				"trace": [
-					"frames": [
-						[
-							"filename": "editor.html",
-							"lineno": lineNumber,
-							"colno": columnNumber
-						]
-					],
-					"exception": [
-						"class": "Error",
-						"message": errorMessage
-					]
+			var data: [String: AnyObject] = [
+				"level": "error",
+				"timestamp": UInt(NSDate().timeIntervalSince1970),
+				"platform": "ios",
+				"language": "javascript",
+				"context": "editor.html",
+				"environment": "production",
+				"notifier": [
+					"name": "canvas-ios"
 				]
 			]
-		} else {
-			data["body"] = [
-				"message": errorMessage
+
+			if let account = account {
+				data["person"] = [
+					"id": account.user.ID,
+					"username": account.user.username,
+					"email": account.email
+				]
+			}
+
+			if let lineNumber = lineNumber, columnNumber = columnNumber {
+				data["body"] = [
+					"trace": [
+						"frames": [
+							[
+								"filename": "editor.html",
+								"lineno": lineNumber,
+								"colno": columnNumber
+							]
+						],
+						"exception": [
+							"class": "Error",
+							"message": errorMessage
+						]
+					]
+				]
+			} else {
+				data["body"] = [
+					"message": errorMessage
+				]
+			}
+
+			if let version = NSBundle.mainBundle().infoDictionary?["CFBundleVersion"] as? UInt {
+				data["code_version"] = version
+			}
+
+			let payload = [
+				"access_token": Config.rollbarToken,
+				"data": data
 			]
-		}
 
-		if let version = NSBundle.mainBundle().infoDictionary?["CFBundleVersion"] as? UInt {
-			data["code_version"] = version
-		}
+			// Add JSON to request
+			request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(payload, options: [])
 
-		let payload = [
-			"access_token": Config.rollbarToken,
-			"data": data
-		]
-
-		// Add JSON to request
-		request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(payload, options: [])
-
-		// Request
-		NSURLSession.sharedSession().dataTaskWithRequest(request).resume()
+			// Request
+			NSURLSession.sharedSession().dataTaskWithRequest(request).resume()
+		#endif
 	}
 }
---
+++ Vendor/CanvasText/CanvasText/Resources/editor.js
@@ -0,0 +1,74 @@
+var Canvas = {};
+window.Canvas = Canvas;
+
+Canvas.connect = function(realtimeURL, accessToken, organizationID, canvasID) {
+  var share = new ShareJSWrapper.default({
+    accessToken: accessToken,
+    canvasID: canvasID,
+    realtimeURL: realtimeURL,
+    orgID: organizationID
+  });
+
+  // Tell the client to connect to the ShareJS server.
+  share.connect(function onConnect() {
+    // Get the current content of the document.
+    Canvas._sendMessage({
+      "snapshot": share.content
+    });
+
+    // Handle an `insert` event from the server.
+    share.on('insert', function onInsert(position, text) {
+      console.log("Remote Insert:", {"location": position, "string": text});
+      Canvas._sendMessage({
+        "op": {
+          "type": "insert",
+          "location": position,
+          "text": text
+        }
+      });
+    });
+
+    // Handle a `remove` event from the server.
+    share.on('remove', function onRemove(position, length) {
+      console.log("Remote Remove:", {"location": position, "length": length});
+      Canvas._sendMessage({
+        "op": {
+          "type": "remove",
+          "location": position,
+          "length": length
+        }
+      });
+    });
+  });
+
+  Canvas._share = share;
+};
+
+Canvas.insert = function(location, string) {
+  console.log("Local Insert:", {"location": location, "string": string});
+  Canvas._share.insert(location, string);
+  return true;
+};
+
+Canvas.remove = function(location, length) {
+  console.log("Local Remove:", {"location": location, "length": length});
+  Canvas._share.remove(location, length);
+  return true;
+};
+
+Canvas._sendMessage = function(message) {
+  console.log(message);
+  if (typeof window.webkit != "undefined" && typeof window.webkit.messageHandlers != "undefined") {
+    window.webkit.messageHandlers.share.postMessage(message);
+  }
+};
+
+window.onerror = function(errorMessage, url, lineNumber, columnNumber) {
+  if (typeof window.webkit != "undefined" && typeof window.webkit.messageHandlers != "undefined") {
+    window.webkit.messageHandlers.share.postMessage({
+      "error": errorMessage,
+      "lineNumber": lineNumber,
+      "columnNumber": columnNumber
+    });
+  }
+};
