d06a832be027cf1ae662434d712975a889bda29c
Sam Soffes <sam@soff.es>
2016-04-18 14:28:35 -0700

    Move environment to its own file

--- Canvas/Configuration.swift
+++ Canvas/Configuration.swift
@@ -14,32 +14,7 @@ private let canvasClientSecretPart2 = "f5bd59c7866e85"
 private let canvasClientSecretPart1 = "60ff40c860274eb9afb6"
 private let canvasClientSecretPart3 = "97bdcc48ae89946"
 
-private enum Environment: String {
-	case Production
-	case Staging
-
-	var baseURL: NSURL {
-		switch self {
-		case .Production: return NSURL(string: "https://api.usecanvas.com/v1/")!
-		case .Staging: return NSURL(string: "https://canvas-api-staging.herokuapp.com/v1/")!
-		}
-	}
-
-	var realtimeURL: NSURL {
-		switch self {
-		case .Production: return NSURL(string: "wss://realtime.usecanvas.com/")!
-		case .Staging: return NSURL(string: "wss://canvas-realtime-staging.herokuapp.com/")!
-		}
-	}
-
-	var presenceURL: NSURL {
-		switch self {
-		case .Production: return NSURL(string: "wss://presence.usecanvas.com/")!
-		case .Staging: return NSURL(string: "wss://canvas-presence-staging.herokuapp.com/")!
-		}
-	}
-}
-
+/// Application environment
 private let env = Environment.Staging
 
 struct Config {
