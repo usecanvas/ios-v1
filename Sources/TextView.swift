7959df2b661bc7bd94494b6eb20344ac6585eb08
Sam Soffes <sam@soff.es>
2016-04-27 16:09:28 -0700

    Fix carets in big selection rects

--- Sources/TextView.swift
+++ Sources/TextView.swift
@@ -35,25 +35,17 @@ class TextView: UITextView {
 			layoutManager.ensureLayoutForTextContainer(textContainer)
 			
 			let characterIndex = offsetFromPosition(beginningOfDocument, toPosition: position)
-			
-			let height: CGFloat
-			
-			if characterIndex == 0 {
-				// Hack for empty document
-				height = 43.82015625
-			} else {
-				if characterIndex == textStorage.length {
-					return rect
-				}
-				
-				let glyphIndex = layoutManager.glyphIndexForCharacterAtIndex(characterIndex)
-				
-				if UInt(glyphIndex) == UInt.max - 1 {
-					return rect
-				}
-				
-				height = layoutManager.lineFragmentUsedRectForGlyphAtIndex(glyphIndex, effectiveRange: nil).size.height
+			if characterIndex == textStorage.length {
+				return rect
 			}
+			
+			let glyphIndex = layoutManager.glyphIndexForCharacterAtIndex(characterIndex)
+			
+			if UInt(glyphIndex) == UInt.max - 1 {
+				return rect
+			}
+			
+			let height = layoutManager.lineFragmentUsedRectForGlyphAtIndex(glyphIndex, effectiveRange: nil).size.height
 			
 			if height > 0 {
 				rect.size.height = height
@@ -61,14 +53,5 @@ class TextView: UITextView {
 		}
 		
 		return rect
-	}
-	
-	// Omit empty width rect when drawing selection rects.
-	override func selectionRectsForRange(range: UITextRange) -> [AnyObject] {
-		let selectionRects = super.selectionRectsForRange(range)
-		return selectionRects.filter({ selection in
-			guard let selection = selection as? UITextSelectionRect else { return false }
-			return selection.rect.size.width > 0
-		})
 	}
 }
