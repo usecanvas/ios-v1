dd6eae84fc1391513cfa78cfba22497d03e218b8
Sam Soffes <sam@soff.es>
2016-07-13 08:20:25 -0600

    Update spacing

--- Sources/OnboardingBillboardViewController.swift
+++ Sources/OnboardingBillboardViewController.swift
@@ -36,6 +36,8 @@ class OnboardingBillboardViewController: StackViewController {
 		return label
 	}()
 	
+	private var textIllustrationSpacing: NSLayoutConstraint!
+	
 	
 	// MARK: - UIViewController
 	
@@ -44,17 +46,29 @@ class OnboardingBillboardViewController: StackViewController {
 		
 		stackView.axis = .Vertical
 		stackView.alignment = .Center
-		stackView.layoutMargins = UIEdgeInsets(top: 48, left: 16, bottom: 16, right: 16)
+		stackView.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16 + 44, right: 16)
 		stackView.layoutMarginsRelativeArrangement = true
 		
 		stackView.addArrangedSubview(titleLabel)
 		stackView.addSpace(12)
 		stackView.addArrangedSubview(subtitleLabel)
-		stackView.addSpace(32)
+		
+		let spacer = UIView()
+		spacer.translatesAutoresizingMaskIntoConstraints = false
+		stackView.addArrangedSubview(spacer)
+		
+		textIllustrationSpacing = spacer.heightAnchor.constraintEqualToConstant(0)
+		textIllustrationSpacing.active = true
+		
 		stackView.addArrangedSubview(illustrationView)
 		
 		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updateFonts), name: UIContentSizeCategoryDidChangeNotification, object: nil)
 		updateFonts()
+	}
+	
+	override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
+		super.traitCollectionDidChange(previousTraitCollection)
+		textIllustrationSpacing.constant = traitCollection.horizontalSizeClass == .Regular ? 48 : 32
 	}
 	
 	
